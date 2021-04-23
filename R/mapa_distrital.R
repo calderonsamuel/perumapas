0164905d231b833bbfb6a7992035ec2fe6f46647
calderonsamuel <samuel.calderon@uarm.pe>
2021-04-23 11:41:34 -0500

    Initial commit

---
+++ R/mapa_distrital.R
@@ -0,0 +1,14 @@
+#' Mapa de límites distritales de Perú
+#'
+#'A simple features dataset containing the limits of the 1874 peruvian districts. The data set was found in www.geogpsperu.com .
+#'
+#' @format A data frame with 1874 rows and 16 variables:
+#' \describe{
+#' \item{departamento}{Department name. First-level administrative division}
+#' \item{provincia}{Province name. Second-level administrative division}
+#' \item{distrito}{District name. Third-level administrative division}
+#' \item{ubigeo}{INEI's geographic location code}
+#' \item{geometry}{Column containing simple features data}
+#' }
+#' @source \url{https://www.geogpsperu.com/2019/05/limite-distrital-actualizado-inei.html}
+"mapa_distrital"
