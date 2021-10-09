b784ac4d152cca905f8dff24c68eb05b63fd848f
calderonsamuel <samuel.calderon@uarm.pe>
2021-10-09 13:24:56 -0500

    add zonas_lima

---
+++ R/zonas_lima.R
@@ -0,0 +1,12 @@
+#' Distritos de Lima por zonas
+#'
+#' A  dataset containing the names and zone of the 43 districts of Lima, Perú.
+#'
+#' @format A data frame with 43 rows and 3 variables:
+#' \describe{
+#' \item{distrito}{District name. Third-level administrative division}
+#' \item{ubigeo}{INEI's geographic location code}
+#' \item{zona}{District zone}
+#' }
+#' @source \url{https://www.mapadelima.com/mapa-de-distritos-de-lima/}
+"zonas_lima"
